541bc4a86b78506c4a16543647d4c8a4210ea89c
kmrak47 <kmrak789704@gmail.com>
2026-05-25 01:52:11 +0530

    push data add

---
+++ test_mongodb.py
@@ -0,0 +1,15 @@
+import certifi
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://kmrak789704_db_user:<password>@cluster0.tpevqou.mongodb.net/?appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
